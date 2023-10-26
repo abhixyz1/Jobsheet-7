7679291e2c3d2fd5ccc7888d9a1bc51d17b42728
abhixyz1 <irfanhendra90@gmail.com>
2023-10-26 21:53:06 +0700

    tugas pertama -modif

--- ForKelipatan20.java
+++ ForKelipatan20.java
@@ -6,18 +6,22 @@ public class ForKelipatan20 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan, jumlah = 0, counter = 0;
+        double rata_rata = 0;
 
         System.out.println("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
+
 
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
                 jumlah += i;    
                 counter++;
+                rata_rata= (double)jumlah/counter;
             }
         }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n ", kelipatan, jumlah);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata rata dari seluruh bilangan kelipatan %d dari 1 sampai 50 adalah %.2f", kelipatan, rata_rata);
 
     }
 }
